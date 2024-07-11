cc721897d2175ca3b54fded71cb23beb120e36b3
VIKASH TIWARI <viktari@amazon.com>
2024-07-11 11:12:25 -0700

    Fix with Zip

--- experiments/binary-quantization-knn-exp/exp-1/run.sh
+++ experiments/binary-quantization-knn-exp/exp-1/run.sh
@@ -27,7 +27,7 @@ source ${EXPERIMENT_PATH}/functions.sh
 
 
 # Derive procedure for indexing and search information
-OSB_INDEX_PROCEDURE="train-test-index-with-merge"
+OSB_INDEX_PROCEDURE="no-train-test-index-with-merge"
 
 # Copy params to OSB folder
 cp ${PARAMS_PATH}/100.json ${OSB_PARAMS_PATH}/
