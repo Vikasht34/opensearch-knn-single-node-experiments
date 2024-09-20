962daeabacd01275d889397fae9cecb8e2fdea6b
VIKASH TIWARI <viktari@amazon.com>
2024-09-19 17:51:28 -0700

    More Tests

--- experiments/binary-quantization-end-to-end-exp/exp-8/run.sh
+++ experiments/binary-quantization-end-to-end-exp/exp-8/run.sh
@@ -29,8 +29,8 @@ source ${EXPERIMENT_PATH}/functions.sh
 
 
 # Derive procedure for indexing and search information
-#OSB_INDEX_PROCEDURE="no-train-test-index-only"
-OSB_INDEX_PROCEDURE="no-train-test-index-with-merge"
+OSB_INDEX_PROCEDURE="no-train-test-index-only"
+#OSB_INDEX_PROCEDURE="no-train-test-index-with-merge"
 
 # Copy params to OSB folder
 cp ${PARAMS_PATH}/100.json ${OSB_PARAMS_PATH}/
